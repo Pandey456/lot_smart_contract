1af1484270c698818fdb3a55f579df50e09c3b19
Adarsh Pandey <pandey.adarsh2910@gmail.com>
2026-03-04 20:29:54 +0530

    gas optimization - reverts

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,56 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
 
-contract Raffle {}
+/**
+ * @title A lottery contract
+ * @author Adarsh
+ * @notice THis contract will create simple raffle contract
+ */
+
+contract Raffle {
+    /** errors */
+    error Raffle__senMoreEthTOEnterRaffle();
+
+    /** Variables */
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee,"not enough ETH to send"); --> this will require more gas
+        // as it has to store this string "not enough ETH to send", so we will use revert
+        if (msg.value < i_entranceFee) {
+            revert Raffle__senMoreEthTOEnterRaffle();
+        }
+    }
+
+    function pickWinner() public {}
+
+    /** Getter function */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
