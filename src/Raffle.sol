7f73f86387eb9e04a690cfdac39e28021e9150e6
Adarsh Pandey <pandey.adarsh2910@gmail.com>
2026-03-07 20:39:46 +0530

    half chainlink VRF

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -21,6 +21,7 @@
 
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
 /**
  * @title A lottery contract
@@ -28,19 +29,24 @@ pragma solidity ^0.8.19;
  * @notice THis contract will create simple raffle contract
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /** errors */
     error Raffle__senMoreEthTOEnterRaffle();
 
     /** Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    //@dev - thie after which the lottery will pick a winner (in seconds)
+    uint256 immutable i_interval;
+    uint256 private s_lastTimeStamp;
 
     /* events */
     event RaffleEntered(address player);
 
-    constructor(uint256 entranceFee) {
-        i_entranceFee = entranceFee;
+    constructor(uint256 _entranceFee, uint256 _interval) {
+        i_entranceFee = _entranceFee;
+        i_interval = _interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() public payable {
@@ -53,7 +59,24 @@ contract Raffle {
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() public {
+        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
+            revert();
+        }
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
+    }
 
     /** Getter function */
     function getEntranceFee() external view returns (uint256) {
