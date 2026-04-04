4396f0e707a2a5af2ac7be4f69487f27867b93aa
Adarsh Pandey <pandey.adarsh2910@gmail.com>
2026-04-04 17:35:23 +0530

    added VRF v1.1.1 and passing constructor

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -8,7 +8,14 @@ contract DeployRaffle is Script {
 
     function run() external {
         vm.startBroadcast();
-        raffle = new Raffle(1);
+        raffle = new Raffle(
+            0.01 ether,
+            30,
+            0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625, // Sepolia VRF Coordinator
+            0, // subId
+            0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // gasLane
+            500000
+        );
 
         vm.stopBroadcast();
     }
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -31,30 +31,45 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
  */
 
 contract Raffle is VRFConsumerBaseV2Plus {
-    /** errors */
+    /**
+     * errors
+     */
     error Raffle__senMoreEthTOEnterRaffle();
 
-    /** Variables */
+    /**
+     * Variables
+     */
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUMWORDS = 1;
     uint256 private immutable i_entranceFee;
+    uint32 private immutable i_callbackGasLimit;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
     address payable[] private s_players;
     //@dev - thie after which the lottery will pick a winner (in seconds)
     uint256 immutable i_interval;
     uint256 private s_lastTimeStamp;
 
     /* events */
-    event RaffleEntered(address player);
+    event RaffleEntered(address indexed player);
 
     constructor(
         uint256 _entranceFee,
         uint256 _interval,
-        address _vrfCoordinator
+        address _vrfCoordinator,
+        uint256 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
     ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
         i_entranceFee = _entranceFee;
         i_interval = _interval;
         s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee,"not enough ETH to send"); --> this will require more gas
         // as it has to store this string "not enough ETH to send", so we will use revert
         if (msg.value < i_entranceFee) {
@@ -64,26 +79,29 @@ contract Raffle is VRFConsumerBaseV2Plus {
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {
+    function pickWinner() external {
         if ((block.timestamp - s_lastTimeStamp) > i_interval) {
             revert();
         }
-        VRFV2PlusClient.RandomWordsRequest request = VRFV2PlusClient
-            .RandomWordsRequest({
-                keyHash: s_keyHash,
-                subId: s_subscriptionId,
-                requestConfirmations: requestConfirmations,
-                callbackGasLimit: callbackGasLimit,
-                numWords: numWords,
-                extraArgs: VRFV2PlusClient._argsToBytes(
-                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
-                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-                )
-            });
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUMWORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
         uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
 
-    /** Getter function */
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {}
+
+    /**
+     * Getter function
+     */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
