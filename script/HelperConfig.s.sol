2d8751c3dd408ff06d81c5d90f6b546e70989c6e
Adarsh Pandey <pandey.adarsh2910@gmail.com>
2026-04-12 18:52:16 +0530

    updated

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -74,5 +74,6 @@ contract HelperConfig is Script, codeConstant {
             gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c, // gasLane
             callbackGasLimit: 500000
         });
+        return localnetworkConfig;
     }
 }
